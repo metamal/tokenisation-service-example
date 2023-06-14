f73414cf8554ff9a8a999c540159f10f0d11c146
Alexander Nikitin <metamal@gmail.com>
2023-06-14 22:59:16 +1200

    Set up Prettier

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!');
 });
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
