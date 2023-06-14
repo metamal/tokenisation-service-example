22419c755b753ddd6add44301da1a4f15ce92dee
Alexander Nikitin <metamal@gmail.com>
2023-06-15 02:31:56 +1200

    Use LokiJS to store tokens

--- src/app.js
+++ src/app.js
@@ -1,31 +1,43 @@
 const express = require('express');
 const crypto = require('crypto');
+const Loki = require('lokijs');
 
-// Create a new express app
+// Create a new Express app
 const app = express();
 
-// Use express's built-in JSON parser
+// Use Express's built-in JSON parser
 app.use(express.json());
+
+// Create a new LokiJS database
+const db = new Loki('tokens.db');
+
+// Add a collection to the database with a unique index on 'acc' and 'token'
+const pairs = db.addCollection('tokens', { unique: ['acc', 'token'] });
 
 function generateHash(input) {
   return crypto.createHash('sha256').update(input).digest('hex');
 }
 
-const globalTokens = {};
-
 app.post('/tokenize', (req, res) => {
   const accountNumbers = req.body;
-  const result = accountNumbers.map((s) => {
-    const token = generateHash(s);
-    globalTokens[token] = s;
-    return token;
+  const result = accountNumbers.map((acc) => {
+    const pair = pairs.by('acc', acc);
+    if (!pair) {
+      const token = generateHash(acc);
+      pairs.insert({ acc, token });
+      return token;
+    }
+    return pair.token;
   });
   res.json(result);
 });
 
 app.post('/detokenize', (req, res) => {
   const inputTokens = req.body;
-  const result = inputTokens.map((token) => globalTokens[token]);
+  const result = inputTokens.map((token) => {
+    const pair = pairs.by('token', token);
+    return pair?.acc ?? null;
+  });
   res.json(result);
 });
 
